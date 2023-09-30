a6e3b1f381c46a61393f247699337bbfc939ca40
Bruno Faria <bruno.fariadevl@gmail.com>
2023-09-30 03:40:01 -0300

    organização de diretorios e interface com zoom e selecao image

---
+++ src/app/image_processing.py
@@ -0,0 +1,91 @@
+import os
+import csv
+import cv2
+import numpy as np
+
+# Diretórios
+IMAGE_DIR = 'D:\GitHub\image-processing\src\images'
+OUTPUT_DIR = 'D:\GitHub\image-processing\src\images-processed'
+CSV_FILE = 'D:\GitHub\image-processing\src\data\classifications.csv'
+
+# Classes
+CLASSES = ['Negative for intraepithelial lesion', 'ASC-US', 'ASC-H', 'LSIL', 'HSIL', 'SCC']
+
+class ImageProcessor:
+    def __init__(self, image_dir, output_dir, image_size):
+        self.image_dir = image_dir
+        self.output_dir = output_dir
+        self.image_size = image_size
+    
+    def create_folders_classes(self, classes):
+        try:
+            for class_name in classes:
+                class_dir = os.path.join(self.output_dir, class_name)
+                os.makedirs(class_dir, exist_ok=True)
+            print('Diretórios criados com sucesso.')
+            return True
+        except Exception as e:
+            raise Exception(f'Erro ao criar diretórios: {str(e)}')
+
+    def processing_images(self, csv_name):
+        with open(csv_name, 'r') as csvfile:
+            csv_reader = csv.DictReader(csvfile)
+            self.processing_image_scale(csv_reader=csv_reader)
+
+    def processing_image_scale(self, csv_reader):
+        try:
+            for row in csv_reader:
+                image_id = int(row['image_id'])
+                image_filename = row['image_filename']
+                cell_id = int(row['cell_id'])
+                bethesda_system = row['bethesda_system']
+                nucleus_x = int(row['nucleus_x'])
+                nucleus_y = int(row['nucleus_y'])
+
+                image_path = os.path.join(self.image_dir, image_filename)
+
+                # Trata a condição de não ter todas images disponiveis
+                if os.path.exists(image_path):
+                    image = cv2.imread(image_path)
+
+                    if image is not None:
+                        x_center = nucleus_x
+                        y_center = nucleus_y
+
+                        # Garante que o recorte esteja sempre centrado no núcleo
+                        x1 = x_center - self.image_size // 2
+                        x2 = x1 + self.image_size
+                        y1 = y_center - self.image_size // 2
+                        y2 = y1 + self.image_size
+
+                        # Ajusta as coordenadas do recorte se estiverem fora dos limites da imagem
+                        if x1 < 0:
+                            x2 -= x1
+                            x1 = 0
+                        if y1 < 0:
+                            y2 -= y1
+                            y1 = 0
+                        if x2 > image.shape[1]:
+                            x1 -= (x2 - image.shape[1])
+                            x2 = image.shape[1]
+                        if y2 > image.shape[0]:
+                            y1 -= (y2 - image.shape[0])
+                            y2 = image.shape[0]
+
+                        # Recorta a imagem usando as coordenadas ajustadas
+                        sub_image = image[y1:y2, x1:x2]
+
+                        class_dir = os.path.join(self.output_dir, bethesda_system)
+                        output_filename = f"{cell_id}.png"
+                        output_path = os.path.join(class_dir, output_filename)
+                        cv2.imwrite(output_path, sub_image)
+        except Exception as e:
+            raise Exception(f'Erro ao criar sub imagens: {str(e)}')
+        
+def main():
+    image_processor = ImageProcessor(image_dir=IMAGE_DIR, output_dir=OUTPUT_DIR, image_size=100)
+    image_processor.create_folders_classes(classes=CLASSES)
+    image_processor.processing_images(csv_name=CSV_FILE)
+
+if __name__ == "__main__":
+    main()
