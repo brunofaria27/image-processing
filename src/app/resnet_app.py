8775709a191d786b0da1db961d50c2f5f616fddd
Bruno Faria <bruno.fariadevl@gmail.com>
2023-11-28 19:44:15 -0300

    inserindo classes verdadeiras no dataframe

--- src/app/resnet_app.py
+++ src/app/resnet_app.py
@@ -1,3 +1,4 @@
+import csv
 import cv2
 import pandas as pd
 import numpy as np
@@ -6,13 +7,13 @@ from tensorflow.keras.preprocessing import image
 from tensorflow.keras.applications.inception_v3 import preprocess_input
 
 def load_and_preprocess_image(image):
-    img_resized = cv2.resize(image, (100, 100), interpolation=cv2.INTER_AREA)
-    img_array = np.expand_dims(img_resized, axis=0)
-    img_array = preprocess_input(img_array)
+    img = cv2.resize(image, (100, 100))
+    img = img / 255.0  # Normalize the image
+    img_array = np.expand_dims(img, axis=0)
     return img_array
 
-def classify_image_binary(model, image):
-    img_array = load_and_preprocess_image(image)
+def classify_image_binary(model, image_path):
+    img_array = load_and_preprocess_image(image_path)
     prediction = model.predict(img_array)[0, 0]
     class_name = "Positive" if prediction > 0.5 else "Negative"
     confidence = max(prediction, 1 - prediction) * 100
@@ -30,29 +31,42 @@ def classify_image_multiclass(model, image):
 
     return class_name, confidence
 
-
-def process_resnet_binary(segmented_images):
+def process_resnet_binary(segmented_images, ids):
     model_path = 'train_codes/ai_models/my_model_binary_resnet.h5'
     model = load_model(model_path)
+    true_classes = search_true_classes(ids)
 
     results = []
-    for image in segmented_images:
+    for image, true_class in zip(segmented_images, true_classes):
         class_name, confidence = classify_image_binary(model, image)
-        results.append([class_name, confidence])
+        results.append([true_class, class_name, confidence])
 
-    columns = ['Predicted Class', 'Confidence']
+    columns = ['True Class', 'Predicted Class', 'Confidence']
     results_df = pd.DataFrame(results, columns=columns)
     return results_df
 
-def process_resnet_multiclass(segmented_images):
+def process_resnet_multiclass(segmented_images, ids):
     model_path = 'train_codes/ai_models/my_model_multiclass_resnet.h5'
     model = load_model(model_path)
-
+    true_classes = search_true_classes(ids)
+    
     results = []
-    for img in segmented_images:
+    for img, true_class in zip(segmented_images, true_classes):
         class_name, confidence = classify_image_multiclass(model, img)
-        results.append([class_name, confidence])
+        results.append([true_class, class_name, confidence])
 
-    columns = ['Predicted Class', 'Confidence']
+    columns = ['True Class', 'Predicted Class', 'Confidence']
     results_df = pd.DataFrame(results, columns=columns)
     return results_df
+
+def search_true_classes(ids):
+    true_classes = []
+
+    with open('data/classifications.csv', newline='', encoding='utf-8') as csvfile:
+        csv_reader = csv.DictReader(csvfile)
+        
+        for row in csv_reader:
+            for id in ids:
+                if row['cell_id'] == str(id):
+                    true_classes.append(row['bethesda_system'])
+    return true_classes
